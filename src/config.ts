8768031cc03dedf35a45def2c7a0d0fd0645bf82
tkhwang <tkhwang@gmail.com>
2024-07-03 07:44:10 +0900

    🔧 build(config): update config

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Tiny and Magnificent",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
