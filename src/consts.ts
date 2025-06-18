3b34cab33ad6ef6dcc09d21100f107e6854a1566
tkhwang <tkhwang@gmail.com>
2025-06-19 08:11:06 +0900

    feat(config): update references with new blog title
    - change SITE_TITLE in consts.txt
    - change title in index.astro

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 export const SITE_URL = "https://tkhwang.github.io";
 export const SITE_TITLE =
-  "tkhwang's tech blog: 비극은 순식간이고, 기적은 오래 걸린다";
+  "tkhwang's tech blog: 나쁜 일은 한 순간에 일어나고, 좋은 일은 오래 걸린다";
 export const SITE_DESCRIPTION =
   "이곳은 변하는 기술 속에서 변하지 않는 개발의 지혜를 기록하는 공간입니다.";
 
